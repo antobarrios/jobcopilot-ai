29ec41a74c57db06451b054f799733076ab0030b
Antonella Barrios <147841161+antobarrios@users.noreply.github.com>
2026-06-16 21:43:19 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -74,7 +74,7 @@ def crear_trabajo(trabajo: Trabajo):
         session.refresh(trabajo)
         return trabajo
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 @app.post("/analizar-cv")
 async def analizar_cv(file:UploadFile,vacante:str):
     try:
